6796073d568761da48b7562d3c96c49546fcb277
artpi <artur.piszek@gmail.com>
2021-11-20 16:31:47 +0100

    Automated code formatting

--- wp-dao.php
+++ wp-dao.php
@@ -1,5 +1,6 @@
 <?php
 namespace Artpi\WPDAO;
+
 /**
  * Plugin Name:     WP DAO
  * Plugin URI:      https://piszek.com
@@ -16,31 +17,30 @@ namespace Artpi\WPDAO;
 
 
 add_action(
-    'rest_api_init',
-    function () {
-        register_rest_route(
-            'wp-dao',
-            '/message-to-sign',
-            array(
-                'methods'             => 'GET',
-                'callback'            => __NAMESPACE__ . '\generate_message',
-            )
-        );
-    }
+	'rest_api_init',
+	function () {
+		register_rest_route(
+			'wp-dao',
+			'/message-to-sign',
+			array(
+				'methods'  => 'GET',
+				'callback' => __NAMESPACE__ . '\generate_message',
+			)
+		);
+	}
 );
 
 function generate_message() {
-    $nonce = wp_create_nonce();
-    $uri = get_site_url();
-    $domain = parse_url( $uri, PHP_URL_HOST );
-    $statement = "Log In with your Ethereum wallet"; // TBD 
-    $version = 1; // Per https://github.com/ethereum/EIPs/blob/9a9c5d0abdaf5ce5c5dd6dc88c6d8db1b130e95b/EIPS/eip-4361.md#example-message-to-be-signed
-    $issued_at = gmdate( 'Y-m-d\TH:i:s\Z' );
-    $address = "test test";
+	$nonce     = wp_create_nonce();
+	$uri       = get_site_url();
+	$domain    = parse_url( $uri, PHP_URL_HOST );
+	$statement = 'Log In with your Ethereum wallet'; // TBD
+	$version   = 1; // Per https://github.com/ethereum/EIPs/blob/9a9c5d0abdaf5ce5c5dd6dc88c6d8db1b130e95b/EIPS/eip-4361.md#example-message-to-be-signed
+	$issued_at = gmdate( 'Y-m-d\TH:i:s\Z' );
+	$address   = 'test test';
 
-
-// This is copy-pasted from https://github.com/ethereum/EIPs/blob/9a9c5d0abdaf5ce5c5dd6dc88c6d8db1b130e95b/EIPS/eip-4361.md#informal-message-template
-$message = "{$domain} wants you to sign in with your Ethereum account:
+	// This is copy-pasted from https://github.com/ethereum/EIPs/blob/9a9c5d0abdaf5ce5c5dd6dc88c6d8db1b130e95b/EIPS/eip-4361.md#informal-message-template
+	$message = "{$domain} wants you to sign in with your Ethereum account:
 {$address}
 
 {$statement}
@@ -50,6 +50,6 @@ Version: {$version}
 Nonce: {$nonce}
 Issued At: {$issued_at}
 ";
-    return $message;
+	return $message;
 }
 
